5d991eac8543a03aa032d12c3ce739a8e5437034
alexrmorris88 <alexrmorris88@gmail.com>
2021-12-18 16:05:06 -0400

    env updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   env: {
-    DB_URI:
-      "mongodb+srv://Alex:ZaqXsw23$@cluster0.w6txq.mongodb.net/tents?retryWrites=true&w=majority",
+    DB_URI: `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_USER_PASSWORD}$@cluster0.w6txq.mongodb.net/tents?retryWrites=true&w=majority`,
   },
   reactStrictMode: true,
 };
