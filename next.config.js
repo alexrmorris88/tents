b41dc8337bf59261c48806bc044fefc86d8b5963
alexrmorris88 <alexrmorris88@gmail.com>
2021-12-18 16:34:25 -0400

    updated mdb pass and env vars

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  env: {
-    DB_URI: `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_USER_PASSWORD}$@cluster0.w6txq.mongodb.net/tents?retryWrites=true&w=majority`,
-  },
   reactStrictMode: true,
 };
