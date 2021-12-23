e81c0d4ecf71a9ad60664307be108bbc5dcd8ec1
alexrmorris88 <alexrmorris88@gmail.com>
2021-12-22 21:20:45 -0400

    added product details and carousel

--- components/products/productDetails.js
+++ components/products/productDetails.js
@@ -1,20 +1,79 @@
+// Next-React Imports
 import React from "react";
 import { useSelector } from "react-redux";
+// UI imports
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Image from "next/image";
+import Carousel from "react-material-ui-carousel";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 // Component Imports
 import Layout from "../layout/Layout";
 
-const productDetails = () => {
+export default function productDetails() {
   const { tent } = useSelector((state) => state.tentDetails);
+  const { name, price, description, images } = tent;
 
   return (
     <Layout title="Products">
-      <div>
-        <h1>{tent.name}</h1>
-        <h1>{tent.price}</h1>
-        <h1>{tent.description}</h1>
-      </div>
+      <Container maxWidth="lg">
+        <Grid alignItems="top" container justifyContent="center" spacing={3}>
+          <Grid
+            item
+            md={6}
+            xs={12}
+            sx={{
+              order: {
+                xs: 2,
+                md: 1,
+              },
+            }}
+          >
+            <Carousel autoPlay={false} controls={true} indicators={true}>
+              {images.map((image) => {
+                return (
+                  <Image
+                    src={image.url}
+                    alt={name}
+                    title={name}
+                    width="100%"
+                    height="100%"
+                    layout="responsive"
+                    objectFit="contain"
+                  />
+                );
+              })}
+            </Carousel>
+          </Grid>
+          <Grid
+            item
+            md={6}
+            sm={8}
+            xs={12}
+            mt={10}
+            sx={{
+              order: {
+                xs: 1,
+                md: 2,
+              },
+            }}
+          >
+            <div>
+              <Typography color="primary" variant="h1">
+                {name}
+              </Typography>
+              <Typography variant="h3">${price}</Typography>
+              <Typography variant="body1">{description}</Typography>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
     </Layout>
   );
-};
-
-export default productDetails;
+}
