928ba6ccbbe44183590caa41346d890121736f43
alexrmorris88 <alexrmorris88@gmail.com>
2022-01-07 12:56:25 -0400

    added in the review routes

--- backend/controllers/tentController.js
+++ backend/controllers/tentController.js
@@ -2,6 +2,7 @@ import Tent from "../modles/Tent";
 import ErrorHandler from "../utils/errorHandler";
 import catchAsyncErrors from "../middlewares/catchAsyncErrors";
 import APIFeatures from "../utils/apiFeatures";
+import { roundToNearestMinutes } from "date-fns";
 
 // Get All Tents
 // Path: /api/tents
@@ -92,4 +93,55 @@ const deleteTentById = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-export { allTents, newTent, getTentById, updateTentById, deleteTentById };
+// Create New Review
+// Path: /api/review
+const createTentReview = catchAsyncErrors(async (req, res, next) => {
+  const { rating, comment, tentID } = req.body;
+
+  const review = {
+    user: req.user._id,
+    firstName: req.user.firstName,
+    lastName: req.user.lastName,
+    rating: Number(rating),
+    comment,
+  };
+
+  const tent = await Tent.findById(tentID);
+
+  const isReviewed = tent.reviews.find(
+    (r) => r.user.toString() === req.user._id.toString()
+  );
+
+  if (isReviewed) {
+    tent.reviews.forEach((review) => {
+      if (review.user.toString() === req.user._id.toString()) {
+        review.comment = comment;
+        review.rating = rating;
+      }
+    });
+  } else {
+    tent.reviews.push(review);
+    tent.numberOfReviews = tent.reviews.length;
+  }
+
+  tent.ratings =
+    tent.reviews.reduce((acc, item) => item.rating + acc, 0) /
+    tent.reviews.length;
+
+  await tent.save({
+    validateBeforeSave: false,
+  });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+export {
+  allTents,
+  newTent,
+  getTentById,
+  updateTentById,
+  deleteTentById,
+  createTentReview,
+};
