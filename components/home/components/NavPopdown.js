9a0b62a701b20ed97087c1ae33c5fd75b4ee6d32
alexrmorris88 <alexrmorris88@gmail.com>
2022-01-27 19:26:18 -0400

    fixing the nav popdown

--- components/home/components/NavPopdown.js
+++ components/home/components/NavPopdown.js
@@ -4,11 +4,16 @@ import React, { useEffect, useState, useRef, createRef, useLayoutEffect } from '
 import {
   Box,
   Typography,
+  Button
 } from "@mui/material";
 import { makeStyles } from '@mui/styles';
 import { styled } from '@mui/material/styles';
+// Components Import
+import { ServicesPopover } from '../../products/utils/ServicesPopover';
 // Icon Imports
 import { Search } from '../../../icons/search'
+import { ChevronDown } from "../../../icons/chevron-down";
+import { ChevronUp } from "../../../icons/chevron-up";
 
 const StyledBox = styled(Box)({
   "&:hover": {
@@ -22,11 +27,46 @@ const StyledBox = styled(Box)({
 
 
 const WhiteTypography = styled(Typography)({
-  color: '#fff'
+  color: '#fff',
 });
 
 
+const ServicesButton = styled(Button)({
+  variant: "text",
+  width: "100%",
+  borderRadius: 0,
+  justifyContent: "flex-start",
+  "&:hover": {
+    backgroundColor: "transparent",
+    borderColor: "text.primary",
+  },
+});
+
 const NavPopdown = ({ RemoveNavRef, eventListner }) => {
+  const anchorRef = useRef(null);
+  const [openPopover, setOpenPopover] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [ChangeImage, setChangeImage] = useState(false)
+  const [SetupFee, setSetupFee] = useState(false)
+  const [DeliveryFee, setDeliveryFee] = useState(false)
+
+  const handleOpenPopover = () => {
+    setOpenPopover(true);
+    setChangeImage(true)
+  };
+
+  const handleClosePopover = () => {
+    setOpenPopover(false);
+    setChangeImage(false)
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
 
@@ -43,12 +83,12 @@ const NavPopdown = ({ RemoveNavRef, eventListner }) => {
         sx={{ 
           display: 'flex', 
           flexDirection: 'row', 
-          justifyContent: 'space-between', 
+          justifyContent: 'space-between',
           border: 1, 
           borderColor: 'divider', 
           borderRadius: 16, 
           backgroundColor: "#fff",
-          width: { xs: '70vw', sm: '60vw', md: '50vw', lg: '40vw', xl: '30vw' }, 
+          width: { xs: '90vw', sm: '60vw', md: '50vw', lg: '40vw', xl: '30vw' }, 
           p: 1,
           }}
         > 
@@ -58,55 +98,41 @@ const NavPopdown = ({ RemoveNavRef, eventListner }) => {
           }}
           sx={{ 
             display: 'flex', 
-            justifyContent: 'center', 
-            flexDirection: 'column', 
-            flexGrow: 1 ,  
-            borderRight: 1,
-            borderColor: 'divider',
-            width: 1
+            justifyContent: "center",
+            flexDirection: 'column',
+            pl: 1, pr: 2,
+            width: 0.5
           }}>
         
-          <Box sx={{ ml: 2, display: 'flex', alignContent: 'center' }}>
+          <Box sx={{ ml: 2, display: 'flex' }}>
               <Typography variant='subtitle2' sx={{ fontSize: '0.7rem' }}>
                 Rental Start
               </Typography>
           </Box>
 
 
-          <Box sx={{ ml: 2, display: 'flex',   alignContent: 'center' }}>
+          <Box sx={{ ml: 2, display: 'flex' }}>
             <Typography variant='subtitle2' sx={{ fontSize: '0.8rem', color: 'text.secondary' }}>
                 Add Dates
             </Typography>
-
           </Box>
 
         </StyledBox>
 
 
-        <Box 
-          sx={{ 
-            mr: 2, 
-            ml: 2, 
-            display: 'flex', 
-            justifyContent: 'center', 
-            flexGrow: 1,  
-            borderColor: 'divider',
-            width: 1
-          }}>
+        <Box sx={{ borderRight: 1, borderColor: 'divider'}} ></Box>
+
 
           <StyledBox 
             onClick={() => {
               console.log("hi")
             }}
             sx={{ 
-              ml: 1, 
               display: 'flex', 
               justifyContent: 'center', 
               flexDirection: 'column', 
-              flexGrow: 1 ,  
-              borderRight: 1, 
-              borderColor: 'divider',
-              
+              pl: 1, pr: 2,
+              width: 0.5
             }}>
 
             <Box sx={{ ml: 2, display: 'flex', alignContent: 'center'}}>
@@ -121,20 +147,76 @@ const NavPopdown = ({ RemoveNavRef, eventListner }) => {
               </Typography>
             </Box>
           </StyledBox>
-        </Box>
 
 
-        <StyledBox sx={{ display: 'flex' }}>
+          <Box sx={{ borderRight: 1, borderColor: 'divider' }} ></Box>
 
-          <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', p:1,  mr: 1, backgroundColor: 'primary.main', borderRadius: 16 }}>
-            <Box sx={{ display: 'flex', mr: 0.3}}>
-              <Search fontSize="small" style={{ color: "#fff" }} />
-            </Box>
 
-            <Box sx={{ display: 'flex'}}>
-              <WhiteTypography sx={{ fontColor: '#fff' }}>Search</WhiteTypography>
-            </Box>
-          </Box>
+        <StyledBox 
+          sx={{ 
+            display: 'flex', 
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            pl: 2,
+            width: 1
+            }}>
+
+            <Box 
+              onClick={handleOpenPopover} 
+              ref={anchorRef} 
+              sx={{ 
+                display: 'flex', 
+                flexDirection:'column',
+                justifyContent: 'center', 
+                width: 1,
+                mr: 1
+                }}>
+                    <Box  
+                      sx={{ 
+                      display: 'flex',     
+                      flexDirection:'row',
+                      justifyContent: 'space-between',
+                      }}>
+
+                        <Box sx={{ display: "flex", alignContent: 'center' }}>
+                          <Typography variant='subtitle2' sx={{ fontSize: '0.7rem' }}>
+                            Services
+                          </Typography>
+                        </Box>
+
+                        <Box sx={{ display: "flex", alignContent: 'center' }}>
+                          {ChangeImage && ChangeImage ? <ChevronUp color='primary' /> : <ChevronDown color="primary" />}
+                        </Box>
+                    </Box>
+
+                      <Box sx={{ display: 'flex', alignContent: 'center', mt: -0.6}}>
+                        <Typography variant='subtitle2' sx={{ fontSize: '0.8rem', color: 'text.secondary' }}>
+                          Add Services
+                        </Typography>
+                      </Box>
+
+                  </Box>
+
+              <ServicesPopover   
+                anchorEl={anchorRef.current}
+                onClose={handleClosePopover}
+                open={openPopover}
+                setSetupFee={setSetupFee}
+                setDeliveryFee={setDeliveryFee}
+              />
+
+
+              <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', p:1,  mr: 1, backgroundColor: 'primary.main', borderRadius: 16 }}>
+
+                <Box sx={{ display: 'flex', mr: 0.3}}>
+                  <Search fontSize="small" style={{ color: "#fff" }} />
+                </Box>
+
+                <Box sx={{ display: 'flex' }}>
+                  <WhiteTypography sx={{ fontColor: '#fff'}}>Search</WhiteTypography>
+                </Box>
+
+              </Box>
 
         </StyledBox>
       </Box>
